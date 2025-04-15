c70447da5184a75379b7c067e5f7164720bc9ee3
lazarus2019 <thaisonbk2019@gmail.com>
2025-04-16 01:07:50 +0700

    feat(data): JSON endpoint

---
+++ src/pages/endpoint.json.ts
@@ -0,0 +1,12 @@
+import { getCollection } from 'astro:content';
+
+const product = await getCollection('product');
+
+export const GET = async ({}) => {
+  return new Response(JSON.stringify(product), {
+    headers: {
+      'content-type': 'application/json',
+    },
+    status: 200,
+  });
+};
